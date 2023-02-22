c6b82f7249fb1c77d0c35c110be170424dd415e7
isaac-w18 <isaacw1218@gmail.com>
2023-02-21 21:53:03 -0500

    Final

--- dem04/dem04/ContentView.swift
+++ dem04/dem04/ContentView.swift
@@ -9,67 +9,141 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var people = 0
+    @State var maxPeople = 30
     @State private var backgroundColor = Color.blue
     
     var body: some View {
-        ZStack {
-            Color.blue
-                .ignoresSafeArea()
+        NavigationView {
             ZStack {
-                let opacity = Double(people) / 30.0
-                Color.red
+                Color.blue
                     .ignoresSafeArea()
-                    .opacity(opacity)
-                VStack {
-                    HStack {
-                        Text("\(people) people")
-                            .font(.largeTitle)
-                            .bold()
-                            .foregroundColor(.white)
-                        Spacer()
-                        Button {
-                            SettingsView()
-                        } label: {
-                            Image(systemName: "gear")
+                ZStack {
+                    let opacity = Double(people) / Double(maxPeople)
+                    Color.red
+                        .ignoresSafeArea()
+                        .opacity(opacity)
+                    VStack {
+                        HStack {
+                            Text("\(people) people")
+                                .font(.largeTitle)
+                                .bold()
+                                .foregroundColor(.white)
+                            Spacer()
+                            NavigationLink(destination: SettingsView(maxPeople: $maxPeople)) {
+                                Image(systemName: "gear")
                                 .font(.largeTitle)
                                 .foregroundColor(.white)
-                        }
-                    }
-                    .padding()
-                    Spacer()
-                    
-                    HStack {
-                        Button {
-                            if(people != 0) {
-                                people -= 1
                             }
-                        } label: {
-                            Image(systemName: "minus")
-                                .font(.largeTitle)
-                                .foregroundColor(.white)
+//                            Button {
+//                                SettingsView()
+//                            } label: {
+//                                Image(systemName: "gear")
+//                                    .font(.largeTitle)
+//                                    .foregroundColor(.white)
+//                            }
                         }
-                        
+                        .padding()
                         Spacer()
                         
-                        
-                        Button {
-                            people += 1
-                        } label: {
-                            Image(systemName: "plus")
-                                .font(.largeTitle)
-                                .foregroundColor(.white)
+                        HStack {
+                            Button {
+                                if(people != 0) {
+                                    people -= 1
+                                }
+                            } label: {
+                                Image(systemName: "minus")
+                                    .font(.largeTitle)
+                                    .foregroundColor(.white)
+                            }
+                            
+                            Spacer()
+                            
+                            
+                            Button {
+                                people += 1
+                            } label: {
+                                Image(systemName: "plus")
+                                    .font(.largeTitle)
+                                    .foregroundColor(.white)
+                            }
                         }
+                        .padding()
                     }
                     .padding()
                 }
-                .padding()
             }
         }
         
     }
     
+    struct SettingsView: View {
+        @Binding var maxPeople: Int
     
-}
+        var body: some View {
+//            NavigationView {
+//                Form {
+//                    HStack {
+//                        Text("Max Occupancy: \(maxPeople)")
+//                        Spacer()
+//                        Button {
+//                            maxPeople-=1
+//                        } label: {
+//                            Image(systemName: "minus")
+//                        }
+//                        Button {
+//                            maxPeople+=1
+//                        } label: {
+//                            Image(systemName: "plus")
+//                        }
+//                    }
+//    //                Section {
+//    //                    Picker("Max Occupancy: \(maxPeople)", selection: $maxPeople) {
+//    //                        Image(systemName: "minus")
+//    //                        Image(systemName: "plus")
+//    //                    }
+//    //                    .pickerStyle(.inline)
+//    //                }
+//                }
+//                .navigationTitle("Settings")
+    //            .background(.black)
+                ZStack {
+                    Color(.black)
+                        .ignoresSafeArea()
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Settings")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .bold()
+                                .padding()
+                            HStack {
+                                Text("Max Occupancy: \(maxPeople)")
+                                Spacer()
+                                Button {
+                                    maxPeople-=1
+                                } label: {
+                                    Image(systemName: "minus")
+                                }
+                                Button {
+                                    maxPeople+=1
+                                } label: {
+                                    Image(systemName: "plus")
+                                }
+                            }
+                            .foregroundColor(.white)
+                            .border(.white)
+                            Spacer()
+                        }
+                        .padding()
+                        Spacer()
+                    }
+                }
+            }
+        }
+    }
+    
+    
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- dem04/dem04/SettingsView.swift
+++ dem04/dem04/SettingsView.swift
@@ -1,52 +1,73 @@
+////
+////  SettingsView.swift
+////  dem04
+////
+////  Created by Isaac Frank on 2/21/23.
+////
 //
-//  SettingsView.swift
-//  dem04
+//import SwiftUI
 //
-//  Created by Isaac Frank on 2/21/23.
+//struct SettingsView: View {
+//    @Binding var maxPeople: Int
+//    
+//    var body: some View {
+//        NavigationView {
+//            Form {
+//                HStack {
+//                    Text("Max Occupancy: \(maxPeople)")
+//                    Spacer()
+//                    Button {
+//                        maxPeople-=1
+//                    } label: {
+//                        Image(systemName: "minus")
+//                    }
+//                    Button {
+//                        maxPeople+=1
+//                    } label: {
+//                        Image(systemName: "plus")
+//                    }
+//                }
+////                Section {
+////                    Picker("Max Occupancy: \(maxPeople)", selection: $maxPeople) {
+////                        Image(systemName: "minus")
+////                        Image(systemName: "plus")
+////                    }
+////                    .pickerStyle(.inline)
+////                }
+//            }
+//            .navigationTitle("Settings")
+////            .background(.black)
+////            ZStack {
+////                Color(.black)
+////                    .ignoresSafeArea()
+////                HStack {
+////                    VStack(alignment: .leading) {
+//////                        NavigationLink(destination: ContentView()) {
+//////                            HStack {
+//////                                Image(systemName: "chevron.left")
+//////                                Text("Back")
+//////                            }
+//////                        }
+////                        Text("Settings")
+////                            .font(.largeTitle)
+////                            .foregroundColor(.white)
+////                            .bold()
+////                            .padding()
+////
+////                        Spacer()
+////
+////                        Spacer()
+////                    }
+////                    .padding()
+////                    Spacer()
+////                }
+////            }
+//        }
+//    }
+//}
 //
-
-import SwiftUI
-
-struct SettingsView: View {
-    @State private var maxPeople = 20
-    
-    var body: some View {
-        ZStack {
-            Color(.black)
-                .ignoresSafeArea()
-            HStack {
-                VStack(alignment: .leading) {
-                        Button {
-                            ContentView()
-                        } label: {
-                            HStack {
-                                Image(systemName: "chevron.left")
-                                Text("Back")
-                            }
-                        }
-                    Text("Settings")
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
-                        .bold()
-                        .padding()
-                    
-                    Spacer()
-                    
-                    Spacer()
-                }
-                .padding()
-                Spacer()
-            }
-            
-            
-            
-            
-        }
-    }
-}
-
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsView()
-    }
-}
+//struct SettingsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SettingsView(maxPeople: Binding<Int>)
+//    }
+//}
